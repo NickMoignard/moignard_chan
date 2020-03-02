2d650e1ad4f12edc35547630a60b8772a32b5690
NickMoignard <nicklmoignard@gmail.com>
2020-03-02 22:53:04 +1100

    added react

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
 
-  unauthenticated do
-    root :to => 'home#index'
-  end
+  # unauthenticated do
+  root :to => 'home#index'
+  # end
   
-  authenticated do
-    root :to => 'dashboard#index'
-  end
+  # authenticated do
+  #   root :to => 'dashboard#index'
+  # end
 end
