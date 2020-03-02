c3ab770babcd1ea1e2fa53f5217bab945e136b25
NickMoignard <nicklmoignard@gmail.com>
2020-03-02 15:36:40 +1100

    added javascript librarys

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+  unauthenticated do
+    root :to => 'home#index'
+  end
+  
+  authenticated do
+    root :to => 'dashboard#index'
+  end
 end
